0ae35aaccd1cca6e0581daca12449c09d6e8d522
Anoop Singh Khera <kheraano@msu.edu>
2020-10-12 15:25:11 -0400

    Added handler for CORS request with url

--- src/main/java/Ford/AccelerateMonitor/api/ProjectController.java
+++ src/main/java/Ford/AccelerateMonitor/api/ProjectController.java
@@ -19,26 +19,20 @@ public class ProjectController {
         this.projectService = projectService;
     }
 
-    //http://35.9.22.64:8888/
-    @CrossOrigin(origins = "http://35.9.22.64:8888/")
     @PostMapping("/addProject")
     public void addProject(@RequestBody Project project) { projectService.addProject(project); }
 
-    @CrossOrigin(origins = "http://35.9.22.64:8888/")
     @GetMapping("/getAllProjects")
     public List<Project> getAllProjects() {
         return projectService.getAllProjects();
     }
 
-    @CrossOrigin(origins = "http://35.9.22.64:8888/")
     @GetMapping(path = "/getProject/{id}")
     public Project getProject(@PathVariable("id") String id){ return projectService.getProject(id); }
 
-    @CrossOrigin(origins = "http://35.9.22.64:8888/")
     @DeleteMapping(path = "/deleteProject/{id}")
     public void deleteProject(@PathVariable("id") String id){ projectService.deleteProject(id); }
 
-    @CrossOrigin(origins = "http://35.9.22.64:8888/")
     @PutMapping(path = "/updateProject/{id}")
     public void updateProject(@PathVariable("id") String id, @RequestBody Project project) { projectService.updateProject(id, project);}
 }
