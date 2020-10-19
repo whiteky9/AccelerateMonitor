59efc3d2beed25982a7cba715af8e32ad3f4048a
kyle <whiteky9@msu.edu>
2020-10-19 18:40:52 -0400

    no longer stores password in database

--- src/main/java/Ford/AccelerateMonitor/model/Member.java
+++ src/main/java/Ford/AccelerateMonitor/model/Member.java
@@ -8,7 +8,7 @@ public class Member {
     public Member(){
         this.firstName = null;
         this.lastName = null;
-        this.password = null;
+        //this.password = null;
         this.email = null;
         this.id= UUID.randomUUID().toString();
         this.role=null;
@@ -26,11 +26,11 @@ public class Member {
     public String getEmail() { return email; }
 
     public void setEmail(String email) { this.email = email; }
-
+/*
     public String getPassword() { return password; }
 
     public void setPassword(String password) { this.password = password; }
-
+*/
     public String getRole() {
         return role;
     }
@@ -56,7 +56,7 @@ public class Member {
     private String firstName;
     private String lastName;
     private String email;
-    private String password;
+    //private String password;
     private String role;
     private String id;
     private Map<String, Object> teams;
