bdecb66a8661905c49180bbecfda1f56c560b3cd
Anoop Singh Khera <kheraano@msu.edu>
2020-10-22 23:03:43 -0400

    Added CORS handler to product controller

--- src/main/java/Ford/AccelerateMonitor/api/ProductController.java
+++ src/main/java/Ford/AccelerateMonitor/api/ProductController.java
@@ -4,14 +4,12 @@ import Ford.AccelerateMonitor.product.Github;
 import Ford.AccelerateMonitor.product.Jenkins;
 import Ford.AccelerateMonitor.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.text.ParseException;
 
+@CrossOrigin(origins = {"http://localhost:8081", "http://35.9.22.64:8888/"})
 @RequestMapping("/products")
 @RestController
 public class ProductController {
